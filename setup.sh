dcf0ac950502a91da45c9508d0125a0be1ca5e01
Arne Stappen <mehh@agoat.xyz>
2018-03-16 11:36:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-# Build and deploy the ingress controller
+# Build and deploy the ingress routing mesh stack
 
 # Let's build the images
-docker build -t agoat/routingmesh-controller:1.0.3 ./build/routingmesh-controller/1.0.3 
-docker build -t agoat/routingmesh-manager:1.0.3 ./build/routingmesh-manager/1.0.3 
+docker build -t agoat/routing-mesh-controller:1.0.4 ./build/routing-mesh-controller/1.0
+docker build -t agoat/routing-mesh-manager:1.0.4 ./build/routing-mesh-manager/1.0 
 
 # Create network separately (as long as the docker cli is not setting the network.name properly)
 docker network create -d overlay --attachable --label com.docker.stack.namespace=routingmesh routingmesh
 
 # Deploy the stack
-docker stack deploy -c docker-stack-deploy.yml routingmesh
+docker stack deploy -c docker-stack-deploy.yml irm
