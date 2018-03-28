23791491071e4f8d68b28f3749ad680563c87992
Arne Stappen <mehh@agoat.xyz>
2018-03-28 09:58:49 +0200

    Update docker to 18.03.0 and nginx to 1.13.10

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # Build and deploy the ingress routing mesh stack
 
 # Let's build the images
-docker build -t agoat/routing-mesh-controller:1.0.4 ./build/routing-mesh-controller/1.0
-docker build -t agoat/routing-mesh-manager:1.0.4 ./build/routing-mesh-manager/1.0 
+docker build -t agoat/routing-mesh-controller:1.1 ./build/routing-mesh-controller/1.1
+docker build -t agoat/routing-mesh-manager:1.1 ./build/routing-mesh-manager/1.1 
 
 # Create network separately (as long as the docker cli is not setting the network.name properly)
 docker network create -d overlay --attachable --label com.docker.stack.namespace=routingmesh routingmesh
